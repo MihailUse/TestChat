a7bc005a307037531f0604be380f8ac26a2fbe21
Kazax_Gld <bezentzev@yandex.ru>
2021-11-24 23:27:07 +0300

    KeyTables

--- src/database/init.ts
+++ src/database/init.ts
@@ -5,11 +5,33 @@ import { Room } from "./models/Room";
 import { Permission } from "./models/Permission";
 import { Massage } from "./models/Massage";
 
+Permission.hasMany(RoomUser, {
+    foreignKey: 'permissionId'
+});
+
+User.hasMany(Massage, {
+    foreignKey: 'userId'
+});
+
+Massage.hasMany(Room, {
+    foreignKey: 'roomId'
+});
+
+Room.hasMany(RoomUser, {
+    foreignKey: 'roomId'
+});
+
+User.hasMany(RoomUser, {
+    foreignKey: 'userId'
+});
+
+
 User.sync({ alter: true })
 RoomUser.sync({ alter: true })
 Room.sync({ alter: true })
 Permission.sync({ alter: true })
 Massage.sync({ alter: true })
+
 
 // sequelizeDB.sync({ alter: true }).then(result => {
 //     console.log(result);
